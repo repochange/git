52a2ff0e7c04d96531450f08b4be48704d04ecf0
den_bezdetnov <552453bezdik@gmail.com>
2020-10-29 10:19:53 -0400

    j

--- src/test/java/com/git/pages/KavaClass.java
+++ src/test/java/com/git/pages/KavaClass.java
@@ -4,9 +4,8 @@ public class KavaClass {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Hello");
-        System.out.println("Conflict");
-        System.out.println("Hello Java");
+
         // a tut toje nu
-        // a nu
+
     }
 }
