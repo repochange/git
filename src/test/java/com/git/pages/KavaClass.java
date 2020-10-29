d43827b22639d7ee29d76dc96d2ceddaf2aa5831
interchange <55008393+repochange@users.noreply.github.com>
2020-10-29 09:58:48 -0400

    Create conflict

--- src/test/java/com/git/pages/KavaClass.java
+++ src/test/java/com/git/pages/KavaClass.java
@@ -2,7 +2,7 @@ package com.git.pages;
 
 public class KavaClass {
     public static void main(String[] args) {
-        System.out.println("Hello java");
         System.out.println("Hello world");
+        System.out.println("Hello");
     }
 }
