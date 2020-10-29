39c3a3d669a609893e96441289ee84f720155639
interchange <55008393+repochange@users.noreply.github.com>
2020-10-29 11:12:47 -0400

    Update KavaClass.java

--- src/test/java/com/git/pages/KavaClass.java
+++ src/test/java/com/git/pages/KavaClass.java
@@ -5,27 +5,12 @@ import java.util.Scanner;
 
 public class KavaClass {
     public static void main(String[] args) {
-
-
-      
-
-
         System.out.println("print");
-       //1
-        
-        //3
-        
-        //4
-        
-        
         //5
-        Scanner scanner = new Scanner(System.in);
-
-        //6
-
         //7
-
-        //9
+        
+        
+      
 
 
 
