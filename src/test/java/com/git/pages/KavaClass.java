fa4a9fc4e214c807231f773104370f045bfbb67e
den_bezdetnov <552453bezdik@gmail.com>
2020-10-29 10:24:38 -0400

    L

--- src/test/java/com/git/pages/KavaClass.java
+++ src/test/java/com/git/pages/KavaClass.java
@@ -4,12 +4,17 @@ public class KavaClass {
     public static void main(String[] args) {
         System.out.println("Hello world");
 
-        System.out.println("Hello");
+
 
         // a tut toje nu
 
 
-        
+        //w
+
+        //t
+
+        //ll
+
 
 
 
