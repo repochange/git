c53873128b88a3cccb5897721ad9a4906bf6434d
interchange <55008393+repochange@users.noreply.github.com>
2020-10-29 10:15:16 -0400

    Update KavaClass.java

--- src/test/java/com/git/pages/KavaClass.java
+++ src/test/java/com/git/pages/KavaClass.java
@@ -3,10 +3,8 @@ package com.git.pages;
 public class KavaClass {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("Hello");
-        System.out.println("Conflict");
-        System.out.println("Hello Java");
-        // a tut toje nu
+        
+
         // a nu
     }
 }
