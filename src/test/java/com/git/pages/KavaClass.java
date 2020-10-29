21256c3b38eca39d5782548d55adbf054d40c8e1
interchange <55008393+repochange@users.noreply.github.com>
2020-10-29 10:11:01 -0400

    a nu

--- src/test/java/com/git/pages/KavaClass.java
+++ src/test/java/com/git/pages/KavaClass.java
@@ -3,8 +3,6 @@ package com.git.pages;
 public class KavaClass {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("Hello");
-        System.out.println("Conflict");
-        System.out.println("Hello Java");
+        // a nu
     }
 }
